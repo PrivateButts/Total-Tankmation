6a71235f08b6175b300736eb5e290f511b072445
unknown <monty845@gmail.comgit config --global user.name Adam>
2015-04-02 17:15:53 -0400

    Started working on an option to have the camera follow your shot

---
+++ Assets/FollowCam.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour {
+	Camera cam;
+	float start;
+	bool followcam = false;
+	// Use this for initialization
+	void Start () {
+		cam = GetComponent<Camera> ();
+		GameObject parent = this.transform.parent.gameObject;
+		//followcam = parent.projTTL;
+		cam.transform.rotation = gameObject.transform.parent.transform.rotation;
+		cam.enabled = false;
+		start = Time.time;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time - start > 0.2 && followcam == true) {
+			cam.enabled = true;
+		}
+	}
+}
