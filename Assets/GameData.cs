918d0cc3ccf584bfa15d122c0ba81c396830194b
Randy Butts <butt6143@fredonia.edu>
2015-04-20 02:06:33 -0400

    Rough draft of Menu Transition Script. Needs data.

--- Assets/GameData.cs
+++ Assets/GameData.cs
@@ -4,20 +4,47 @@ using System.Collections;
 public class GameData : MonoBehaviour {
 	private float TimerStart;
 	private bool FirstTrans, SecondTrans;
-	public float FirstTransTimer, SecondTransTimer;
+
+	public float FirstTransTime, SecondTransTime;
+	public Vector3 TransStart, TransMiddle, TransEnd;
+	public Quaternion TransRStart, TransREnd;
+	public Transform Camera;
+
 
 	// This keeps the Object in the scene unless explictly destroyed
 	void Awake() {
 		DontDestroyOnLoad(transform.gameObject);
+
 	}
+
 
 	// These functions handle the menu transistion, pulling user data, and switching scenes
 	public void OnPlayButtonClicked(){
-		Debug.Log ("Play Hit");
+		TimerStart = Time.time;
+		FirstTrans = true;
 	}
 
 	public void OnGoButtonClicked(){
-		Debug.Log ("Go Hit");
+
+	}
+
+	void Update(){
+		if (FirstTrans) {
+			if (Camera.position != TransMiddle) {
+				Camera.position = Vector3.Lerp (TransStart, TransMiddle, (Time.time - TimerStart) / FirstTransTime);
+			} else {
+				TimerStart = Time.time;
+				FirstTrans = false;
+				SecondTrans = true;
+			}
+		}else if (SecondTrans) {
+			if (Camera.position != TransEnd) {
+				Camera.position = Vector3.Lerp (TransStart, TransMiddle, (Time.time - TimerStart) / FirstTransTime);
+				Camera.rotation = Quaternion.Lerp(TransRStart, TransREnd, (Time.time - TimerStart) / FirstTransTime);
+			} else {
+				SecondTrans = false;
+			}
+		}
 	}
 
 
