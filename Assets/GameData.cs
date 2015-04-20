9b813bcebbe13dbb0c67f35f0c0091caed17045e
Randy Butts <butt6143@fredonia.edu>
2015-04-20 01:50:50 -0400

    Done Prototyping!

--- Assets/GameData.cs
+++ Assets/GameData.cs
@@ -2,10 +2,24 @@
 using System.Collections;
 
 public class GameData : MonoBehaviour {
+	private float TimerStart;
+	private bool FirstTrans, SecondTrans;
+	public float FirstTransTimer, SecondTransTimer;
+
 	// This keeps the Object in the scene unless explictly destroyed
 	void Awake() {
 		DontDestroyOnLoad(transform.gameObject);
 	}
+
+	// These functions handle the menu transistion, pulling user data, and switching scenes
+	public void OnPlayButtonClicked(){
+		Debug.Log ("Play Hit");
+	}
+
+	public void OnGoButtonClicked(){
+		Debug.Log ("Go Hit");
+	}
+
 
 	// This is were all the logic for setting a loaded level with the data in this object
 	void OnLevelWasLoaded(int Level){
