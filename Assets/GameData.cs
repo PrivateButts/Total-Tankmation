6bf4fe0473248e4d682e16ee3fc66e182c8b862b
Randy Butts <butt6143@fredonia.edu>
2015-04-20 02:33:17 -0400

    Split off transition script

--- Assets/GameData.cs
+++ Assets/GameData.cs
@@ -1,14 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameData : MonoBehaviour {
-	private float TimerStart;
-	private bool FirstTrans, SecondTrans;
-
-	public float FirstTransTime, SecondTransTime;
-	public Vector3 TransStart, TransMiddle, TransEnd;
-	public Quaternion TransRStart, TransREnd;
-	public Transform Camera;
+	public Text PlayersIn, AIsIn;
+	public int Players, AIs;
 
 
 	// This keeps the Object in the scene unless explictly destroyed
@@ -17,38 +13,11 @@ public class GameData : MonoBehaviour {
 
 	}
 
-
-	// Menu Transition
-	public void OnPlayButtonClicked(){
-		TimerStart = Time.time;
-		FirstTrans = true;
-	}
-
-	void Update(){
-		if (FirstTrans) {
-			if (Camera.position != TransMiddle) {
-				Camera.position = Vector3.Lerp (TransStart, TransMiddle, (Time.time - TimerStart) / FirstTransTime);
-			} else {
-				TimerStart = Time.time;
-				FirstTrans = false;
-				SecondTrans = true;
-			}
-		}else if (SecondTrans) {
-			if (Camera.rotation != TransREnd) {
-				Camera.position = Vector3.Lerp (TransMiddle, TransEnd, (Time.time - TimerStart) / SecondTransTime);
-				Camera.rotation = Quaternion.Lerp(TransRStart, TransREnd, (Time.time - TimerStart) / SecondTransTime);
-			} else {
-				SecondTrans = false;
-			}
-		}
-	}
-
+	
 	// Grabbing Data and Switching Scenes
 	public void OnGoButtonClicked(){
 
 	}
-
-
 
 
 	// This is were all the logic for setting a loaded level with the data in this object
