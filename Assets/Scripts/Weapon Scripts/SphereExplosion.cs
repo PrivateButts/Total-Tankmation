178af5c59537f2aa165882a6d1c988cf4d1aaa61
Randy Butts <butt6143@fredonia.edu>
2015-04-30 01:34:46 -0400

    Fixed and tweeked explosion script

--- Assets/Scripts/Weapon Scripts/SphereExplosion.cs
+++ Assets/Scripts/Weapon Scripts/SphereExplosion.cs
@@ -4,25 +4,30 @@ using System.Collections;
 public class SphereExplosion : MonoBehaviour {
 	float created;
 	public float explosionsize = 5;
-	public float expansion = .2F;
+	float expansion = .2F;
 	float maxsize = 4.8F;
 
 	public AudioSource expSFX;
 
 	// Use this for initialization
 	void Start () {
-		expansion = explosionsize / 25F;
+		expansion = explosionsize / 50F;
 		maxsize = explosionsize - 0.2F;
 		created = Time.time;
 		expSFX.time = 0;
 		expSFX.Play ();
 
 	}
-		
 	// Update is called once per frame
 	void Update () {
+		Material mat = gameObject.renderer.material;
 		transform.localScale = Vector3.Lerp (new Vector3 (1f, 1f, 1f), new Vector3 (maxsize, maxsize, maxsize), Time.time - created);
-		if(Time.time > created + 2){
-			Destroy(gameObject);
+		if (Time.time > created + .25f) {
+			if(mat.GetFloat("_ClipRange") <=0){
+				Destroy(gameObject);
+				return;
+			}
+			mat.SetFloat("_ClipRange", mat.GetFloat("_ClipRange") -.01f);
+		}
 	}
 }
